9ad46d68f4f7476af5febad30e8de2a7b5535211
Sanjit Sarda <sanjitsarda4@gmail.com>
2019-10-28 12:10:45 -0700

    Change lots of stuff

---
+++ contributing.md
@@ -0,0 +1,3 @@
+# Contributing
+
+If you want to improve the design of the site, clone the repo and open a pull request, but if you wanna add material, then just [	email me](sanjitsarda4@gmial.com).
--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-const b = "ed";
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+! Enable this function when there are more humans
+var githubUrl = "https://api.github.com/repos/sanjScioly/sanjScioly.github.io/contributors";
+
+var contributors = [];
+var requestThing = new XMLHttpRequest();
+requestThing.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+        var collab = JSON.parse(this.responseText);
+        collab.forEach((item, index) => {
+            contributors.push(item);
+        });
+    }
+};
+requestThing.open("GET", githubUrl, true);
+requestThing.send();
+
+// * loop through the list of collaborators and add stuff
+
+console.log(contributors);
+$(document).ready(function () {
+    contributors.forEach((item, index) => {
+        var imageString = '<img src="' + item.avatar_url + '" class="contributor" onClick="window.location.assign(\'' + item.html_url + '\')" height="40">';
+        document.getElementById("contributors").innerHTML += imageString;
+    });
+});
+
+*/
