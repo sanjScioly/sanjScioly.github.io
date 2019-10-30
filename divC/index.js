50ba96e3d5dce42c6f11fac16207b3fe809d97e4
Sanjit Sarda <sanjitsarda4@gmail.com>
2019-10-30 14:56:59 -0700

    stuff for db

---
+++ divC/index.js
@@ -0,0 +1,4 @@
+$(document).ready(function () {
+    alert("All I have so far is Detector Building, So Im gonna redirect you there.");
+    window.location.assign("./DetectorBuilding/index.html");
+});
