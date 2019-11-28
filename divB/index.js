d0f8f1962d5254673be91017f4caf9c69c84e04d
Sanjit Sarda <sanjitsarda4@gmail.com>
2019-11-27 21:49:56 -0800

    stuff

--- divB/index.js
+++ divB/index.js
@@ -1,4 +1,4 @@
-$(document).ready(function () {
-    alert("All I have so far is Circuit Lab, So Im gonna redirect you there.");
-    window.location.assign("./CircuitLab/index.html");
-});
+// //$(document).ready(function () {
+// //    alert("All I have so far is Circuit Lab, So Im gonna redirect you there.");
+// //    window.location.assign("./CircuitLab/index.html");
+// //});
