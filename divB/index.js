0c2c160051b719a63765acd3651016ba5b442ac7
Sanjit Sarda <sanjitsarda4@gmail.com>
2019-11-29 13:58:17 -0800

    Thats a ton of files

--- divB/index.js
+++ divB/index.js
@@ -1,3 +1,17 @@
+function ham() {
+    if (Array.from(document.getElementById("ulNav").classList).includes("hide")) {
+        document.getElementById("ulNav").classList.remove("hide");
+    } else {
+        document.getElementById("ulNav").classList = "hide";
+    }
+}
+
+
+
+
+
+
+
 // //$(document).ready(function () {
 // //    alert("All I have so far is Circuit Lab, So Im gonna redirect you there.");
 // //    window.location.assign("./CircuitLab/index.html");
